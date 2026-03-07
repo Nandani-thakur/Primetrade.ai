8f333e40765efa259bf5b09b3101250c8ec148d5
Nandani Thakur <nandanith7563@gmail.com>
2026-03-07 13:11:24 +0530

    enable cors for all origins

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,3 @@
-// const express=require("express")
-// const cors=require("cors")
-
-// const authRoutes=require("./routes/authRoutes")
-// const taskRoutes=require("./routes/taskRoutes")
-
-// const swaggerUI=require("swagger-ui-express")
-// const swaggerSpec=require("./swagger")
-
-// const app=express()
-
-// app.use(cors())
-// app.use(express.json())
-
-// app.use("/api/v1/auth",authRoutes)
-// app.use("/api/v1/tasks",taskRoutes)
-
-// app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerSpec))
-
-// module.exports=app
-
-
 const express = require("express");
 const cors = require("cors");
 
@@ -31,15 +9,10 @@ const swaggerSpec = require("./swagger");
 
 const app = express();
 
-
-app.use(cors({
-  origin: "https://primetrade-ai-2ss5.vercel.app", // frontend deployed link
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+// Allow all origins
+app.use(cors());
 
 app.use(express.json());
-
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/tasks", taskRoutes);
