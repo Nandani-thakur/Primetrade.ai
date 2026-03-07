f32c99824503045b197eb1939461e02773de632b
Nandani Thakur <nandanith7563@gmail.com>
2026-03-07 06:39:08 +0530

    update-

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,32 +1,17 @@
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:5000/api/v1"
-// });
-
-// API.interceptors.request.use((req) => {
-
-//   const token = localStorage.getItem("token");
-
-//   if (token) {
-//     req.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   return req;
-// });
-
-// export default API;
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://primetrade-ai-nyor.vercel.app/api/v1" 
+  baseURL: "http://localhost:5000/api/v1"
 });
 
 API.interceptors.request.use((req) => {
+
   const token = localStorage.getItem("token");
+
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
+
   return req;
 });
 
