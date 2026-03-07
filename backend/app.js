50b45e664b0449ca0ba63cd156c36cf0baa77a14
Nandani Thakur <nandanith7563@gmail.com>
2026-03-07 12:52:24 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,48 @@
-const express=require("express")
-const cors=require("cors")
+// const express=require("express")
+// const cors=require("cors")
 
-const authRoutes=require("./routes/authRoutes")
-const taskRoutes=require("./routes/taskRoutes")
+// const authRoutes=require("./routes/authRoutes")
+// const taskRoutes=require("./routes/taskRoutes")
 
-const swaggerUI=require("swagger-ui-express")
-const swaggerSpec=require("./swagger")
+// const swaggerUI=require("swagger-ui-express")
+// const swaggerSpec=require("./swagger")
 
-const app=express()
+// const app=express()
 
-app.use(cors())
-app.use(express.json())
+// app.use(cors())
+// app.use(express.json())
 
-app.use("/api/v1/auth",authRoutes)
-app.use("/api/v1/tasks",taskRoutes)
+// app.use("/api/v1/auth",authRoutes)
+// app.use("/api/v1/tasks",taskRoutes)
 
-app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerSpec))
+// app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerSpec))
 
-module.exports=app
+// module.exports=app
+
+
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
+const swaggerUI = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
+
+const app = express();
+
+
+app.use(cors({
+  origin: "https://primetrade-ai-jtt8.vercel.app", // frontend deployed link
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.use(express.json());
+
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/tasks", taskRoutes);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+module.exports = app;
